c68d1fd0aceebacd12a81c360ded39c58249f8bc
Antoine Charnoz <aecz@gft.com>
2015-01-27 17:08:08 +0100

    - Removed unneeded globals module - Removed unneeded grunt-cli - Changed hostname to allow external access

--- Gruntfile.js
+++ Gruntfile.js
@@ -154,8 +154,7 @@ module.exports = function (grunt) {
             options: {
                 protocol: 'http',
                 port: 9000,
-                // Change this to '0.0.0.0' to access the server from outside.
-                hostname: 'localhost'
+                hostname: '*'
             },
             livereload: {
                 options: {
